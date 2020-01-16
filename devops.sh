78c434f49966fec33c79aa706c44f1614f150566
YRollHid <34071152+YRollHid@users.noreply.github.com>
2020-01-16 18:08:57 +0100

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -34,9 +34,9 @@ echo "extracted"
 echo "dependencies installed"
 sudo -u azure_devops ./config.sh --unattended --deploymentgroup --deploymentgroupname $1 --url $2 --auth pat --token $3 --agent $4 --projectname $5 --addDeploymentGroupTags --deploymentGroupTags $6 --acceptTeeEula --work ./_work --runAsService
 echo "configuration done"
-sudo -u azure_devops ./svc.sh install
+sudo ./svc.sh install
 echo "service installed"
-sudo -u azure_devops ./svc.sh start
+sudo ./svc.sh start
 echo "service started"
 echo "config done"
 exit 0
