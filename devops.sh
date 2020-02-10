7bb1bb3c82efe361d8feb12f9864b0ca513140ae
YRollHid <34071152+YRollHid@users.noreply.github.com>
2020-02-10 20:59:34 +0100

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -24,6 +24,7 @@ PRIKEY=$1
 echo ${PRIKEY} > /home/azure_devops/.ssh/id_rsa
 chown azure_devops.azure_devops /home/azure_devops/.ssh/id_rsa
 chmod 600 /home/azure_devops/.ssh/id_rsa
+sed -i 's/ /\n/g' /home/azure_devops/.ssh/id_rsa
 
 PUBKEY=$2
 echo ${PUBKEY} > /home/azure_devops/.ssh/id_rsa.pub
