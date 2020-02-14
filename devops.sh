f31bcbb4dd476556986c640c38d0fdab6e58ec24
YRollHid <34071152+YRollHid@users.noreply.github.com>
2020-02-14 16:08:31 +0100

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -10,6 +10,14 @@ echo "Installing Ansible by python3-pip..."
 sudo -H pip3 install ansible > /tmp/pip_install_ansible
 echo "Installing Atop for Monitoring..."
 sudo apt install -y atop
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt update && sudo apt install -y google-cloud-sdk > /tmp/apt_install_google-cloud-sdk 
 
 echo "Adding Azure_DevOps User..."
 adduser --quiet --disabled-password --shell /bin/bash --home /home/azure_devops --gecos "Azure Agent DevOps" azure_devops
