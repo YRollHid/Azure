45a69c9264cf217b5ec27eeb2fad69e4a331380e
YRollHid <34071152+YRollHid@users.noreply.github.com>
2020-03-13 17:44:50 +0100

    Update post-deploy-chef-nodes.sh

--- post-deploy-chef-nodes.sh
+++ post-deploy-chef-nodes.sh
@@ -4,21 +4,21 @@ echo "Running yum update..."
 sudo yum update -y > /tmp/yum_update
 
 echo "Adding Azure_DevOps User..."
-adduser --quiet --disabled-password --shell /bin/bash --home /home/azure_devops --gecos "Azure Agent DevOps" azure_devops
+sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/azure_devops --gecos "Azure Agent DevOps" azure_devops
 echo "Adding Azure_DevOps user to sudoers..."
-echo -e 'azure_devops\tALL=(ALL)\tNOPASSWD:\tALL' > /etc/sudoers.d/azure_devops
+sudo echo -e 'azure_devops\tALL=(ALL)\tNOPASSWD:\tALL' > /etc/sudoers.d/azure_devops
 
-mkdir /home/azure_devops/.ssh
-chown azure_devops.azure_devops /home/azure_devops/.ssh
-chmod 700 /home/azure_devops/.ssh
+sudo mkdir /home/azure_devops/.ssh
+sudo chown azure_devops.azure_devops /home/azure_devops/.ssh
+sudo chmod 700 /home/azure_devops/.ssh
 
 PUBKEY=$1
-echo ${PUBKEY} > /home/azure_devops/.ssh/authorized_keys
-chown azure_devops.azure_devops /home/azure_devops/.ssh/authorized_keys
-chmod 644 /home/azure_devops/.ssh/authorized_keys
+sudo echo ${PUBKEY} > /home/azure_devops/.ssh/authorized_keys
+sudo chown azure_devops.azure_devops /home/azure_devops/.ssh/authorized_keys
+sudo chmod 644 /home/azure_devops/.ssh/authorized_keys
 
 ## add a swap file in Linux Azure virtual machines ##
-sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
-sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
-sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=2048/g' /etc/waagent.conf
-shutdown -r +1
+sudo sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
+sudo sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
+sudo sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=2048/g' /etc/waagent.conf
+sudo shutdown -r +1
